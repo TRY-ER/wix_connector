08c14dcbe61513e493984a52efed796d15199ddc
try-er <avyarth00@gmail.com>
2024-02-02 22:01:48 +0530

    modfied classes to respond to the client properly with inserting and data operations

--- wix_connector/client.py
+++ wix_connector/client.py
@@ -1,5 +1,5 @@
 from pydantic import (BaseModel,
-                     root_validator)
+                      model_validator)
 from abc import ABC, abstractmethod
 import requests
 from wix_connector.URLs.base import BaseUrl
@@ -23,39 +23,41 @@ class BaseClient(BaseModel, ABC):
 
 class DataClient(BaseClient):     
     wix_site_id: str
+    headers: dict = {} 
 
-    @root_validator(pre=True)
+
+    @model_validator(mode="after") 
     def form_header(cls, values):
-        values["headers"] = {"Authorization": values["authorization"],
-                             "Content-Type": values["content_type"],
-                             "wix-site-id": values["wix_site_id"]}
+        values.headers = {"Authorization": values.authorization,
+                             "Content-Type": values.content_type,
+                             "wix-site-id": values.wix_site_id}
         return values
 
 
     def get(self, url: BaseUrl, param: str | None = None):
         if url.request_type == "GET":
-            response = requests.get(BaseUrl.get_url(param=param) if param else BaseUrl.get_url(), headers=self.headers)
+            response = requests.get(url.get_url(param=param) if param else BaseUrl.get_url(), headers=self.headers)
             return BaseClientResponse(status_code=response.status_code, response=response.json())
         else:
             raise ValueError(f"Invalid request type for this method: {url.request_type}")
 
     def post(self, url: BaseUrl, data: dict, **kwargs):
         if url.request_type == "POST":
-            response = requests.post(BaseUrl.get_url(), json=data, headers=self.headers)
+            response = requests.post(url.get_url(), json=data, headers=self.headers)
             return BaseClientResponse(status_code=response.status_code, response=response.json())
         else:
             raise ValueError(f"Invalid request type for this method: {url.request_type}")
 
     def put(self, url: BaseUrl, data: dict, param: str | None = None):
         if url.request_type == "PUT": 
-            response = requests.put(BaseUrl.get_url(param=param) if param else BaseUrl.get_url(), headers=self.headers) 
+            response = requests.put(url.get_url(param=param) if param else BaseUrl.get_url(), headers=self.headers) 
             return BaseClientResponse(status_code=response.status_code, response=response.json())
         else:
             raise ValueError(f"Invalid request type for this method: {url.request_type}")
 
     def delete(self, url: BaseUrl, data: dict, param: str | None = None):
         if url.request_type == "DELETE": 
-            response = requests.delete(BaseUrl.get_url(param=param) if param else BaseUrl.get_url(),headers=self.headers) 
+            response = requests.delete(url.get_url(param=param) if param else BaseUrl.get_url(),headers=self.headers) 
             return BaseClientResponse(status_code=response.status_code, response=response.json())
         else:
             raise ValueError(f"Invalid request type for this method: {url.request_type}")
@@ -66,7 +68,7 @@ class DataClient(BaseClient):
 class AccountClient(BaseClient):
     wix_account_id: str
 
-    @root_validator(pre=True)
+    @model_validator(mode="after")
     def form_header(cls, values):
         values["headers"] = {"Authorization": values["authorization"],
                              "Content-Type": values["content_type"],
---
+++ wix_connector/data_handler/base.py
@@ -0,0 +1,53 @@
+from pydantic import (BaseModel,
+                     root_validator)
+from abc import ABC, abstractmethod
+from wix_connector.client import DataClient
+from wix_connector.URLs.data.collections import (InsertDataUrl,
+                                                 UpdateDataUrl,
+                                                 SaveDataUrl,
+                                                 GetDataUrl,
+                                                 RemoveDataUrl,
+                                                 QueryDataUrl)
+
+class BaseDataHandler(BaseModel, ABC):
+    client: DataClient 
+
+    def insert_data(self, data:dict):
+        url = InsertDataUrl
+        response = self.client.post(url=url,
+                         data=data)
+        return response
+
+    def update_data(self, data:dict, id: str):
+        url = UpdateDataUrl
+        response = self.client.put(url=url,
+                         data=data,
+                         param=id)
+        return response
+    
+    def save_data(self, data:dict):
+        "this inserts if data doesn't exist if it exists if updates it"
+        url = SaveDataUrl
+        response = self.client.post(url=url,
+                                    data=data)
+        return response 
+
+    def get_data(self, id: str):
+        url = GetDataUrl
+        response = self.client.get(url=url,
+                                   param=id)
+        return response
+
+    def remove_data(self, id: str):
+        url = RemoveDataUrl
+        response = self.client.delete(url=url,
+                                      param=id)
+        return response 
+
+    def query_data(self, query_dict: dict):
+        url = QueryDataUrl
+        response = self.client.post(url=url,
+                                    data=query_dict)
+        return response 
+
+    
